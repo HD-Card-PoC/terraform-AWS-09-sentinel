bc103894a2c458dd7538ded994d4ae4a2d65380d
MZC01-MYNECK <mzc01-myneck@MZC01-MYNECK.local>
2024-03-11 23:36:18 +0900

    2

---
+++ terraform-AWS-09-sentinel-sg/network.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "hdcard-vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "hdcard-poc"
+  }
+}
+
+resource "aws_subnet" "hdcard-subnet" {
+  vpc_id            = aws_vpc.hdcard-vpc.id
+  cidr_block        = "172.16.1.0/24"
+
+  tags = {
+    Name = "hdcard-poc"
+  }
+}
+
+resource "aws_security_group" "hdcard-sg" {
+  name        = "hdcard-sg"
+  vpc_id      = aws_vpc.hdcard-vpc.id
+
+  ingress {
+    description      = "ssh"
+    from_port        = 0
+    to_port          = 22
+    protocol         = "tcp"
+    # cidr_blocks      = [aws_vpc.hdcard-vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = [aws_vpc.hdcard-vpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "hdcard-poc"
+  }
+}
